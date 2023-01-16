17f5ba4b63e01757e94bbeed10ffc5d9344920b5
mdzohairansari <ansarizo@sheridancollege.ca>
2023-01-16 15:27:26 -0500

    week 2 student list

---
+++ src/studentlistprint/StudentListPrint.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlistprint;
+
+public class StudentListPrint {
+    public static void main(String[] args) 
+    {
+        // store and print the studdent list sdfgsdf
+        Student[] list = new Student[3]; //declaration Array of Object
+        Student s1 = new Student();
+        s1.setName("peter");
+        s1.setAge(21);
+        Student s2 = new Student();
+        s2.setName("john");
+        s2.setAge(21);
+        Student s3 = new Student();
+        s3.setName("preet");
+        s3.setAge(21);
+        list[0]=s1;
+        list[1]=s2;
+        list[2]=s3;
+        for(int i=0;i<list.length;i++)
+        {
+            System.out.println(list[i].getName() + " " + list[i].getAge());
+        }
+    }
+    
+}
